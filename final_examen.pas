63ad949361ec2d062a4bcad753a08de3d539fa8c
Diocleniax789 <julioramdom123@gmail.com>
2024-07-23 14:12:07 -0300

    se creo el menu principal y se corrigieron errores en las declaraciones de variables de tipo archivo

--- final_examen.pas
+++ final_examen.pas
@@ -10,24 +10,22 @@ TYPE
 
     profesional = RECORD
      alumno: integer;
-     codigo_asignatura = string [6];
+     codigo_asignatura: string [6];
      nota: real;
     END;
 
     juntos = RECORD
      alumno: integer;
-     codigo_asignatura = string [6];
+     codigo_asignatura: string [6];
      nota: real;
     END;
 
-    archivo_basico = FILE OF basico;
-
-    archivo_profesional = FILE OF profesional;
-
-    archivo_juntos = FILE OF juntos;
 VAR
-registro_basico: basico;
-registro_profesional: profesional;
+   archivo_basico: FILE OF basico;
+   archivo_profesional: FILE OF profesional;
+   archivo_juntos: FILE OF juntos;
+   registro_basico: basico;
+   registro_profesional: profesional;
 
 PROCEDURE crea_archivo_basico;
  BEGIN
@@ -47,10 +45,33 @@ PROCEDURE crea_archivo_juntos;
  close(archivo_juntos);
  END;
 
+PROCEDURE menu_principal;
+VAR
+ op: integer;
+ BEGIN
+ REPEAT
+ textcolor(white);
+ clrscr;
+ writeln('1. Generar listado de alumnos de ambos cursos.');
+ writeln('2. Generar listado de promedios por alumno.');
+ writeln('3. Salir.');
+ writeln();
+ writeln('---------------------');
+ write('Seleccione opcion: ');
+ readln(op);
+ CASE op OF
+  1:BEGIN
+    END;
+  2:BEGIN
+    END;
+ END;
+ UNTIL (op = 3);
+ END;
+
 BEGIN
 assign(archivo_basico,'C:\Users\JULIO\Desktop\final-notas-alumnos\basico.dat');
 assign(archivo_profesional,'C:\Users\JULIO\Desktop\final-notas-alumnos\profesional.dat');
-assing(archivo_juntos,'C:\Users\JULIO\Desktop\final-notas-alumnos\juntos.dat');
+assign(archivo_juntos,'C:\Users\JULIO\Desktop\final-notas-alumnos\juntos.dat');
 crea_archivo_basico;
 crea_archivo_profesional;
 crea_archivo_juntos;
